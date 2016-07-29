78eae746c5323f9979e5ce7d7816204936c40a13
Florian Morel <florian.morel@wire.com>
2016-07-29 10:24:35 +0200

    Make ocmock ios buildable and carthage compatible

--- Source/OCMock/OCMock.h
+++ Source/OCMock/OCMock.h
@@ -3,6 +3,15 @@
 //  Copyright (c) 2004-2008 by Mulle Kybernetik. See License file for details.
 //---------------------------------------------------------------------------------------
 
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZMTesting.
+FOUNDATION_EXPORT double ZMTestingVersionNumber;
+
+//! Project version string for ZMTesting.
+FOUNDATION_EXPORT const unsigned char ZMTestingVersionString[];
+
+
 #import <OCMock/OCMockObject.h>
 #import <OCMock/OCMockRecorder.h>
 #import <OCMock/OCMConstraint.h>
